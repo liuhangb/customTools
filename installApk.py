a90aadf0ac08a68f24dcd7688089a728ad299ec8
lh9 <>
2023-01-06 14:52:15 +0800

    格式化代码

--- installApk.py
+++ installApk.py
@@ -15,14 +15,14 @@ for ele in ctArr:
 
     deviceArr.append(ele.split("\tdevice")[0])
 
-print ("可选择的设备:" + str(deviceArr))
+print("可选择的设备:" + str(deviceArr))
 # python 2.x中input如果需要接收str类型，必须输入时带"", 否则会报错
 # raw_input会将所有输入都当做字符串, 不需要输入时带""
 # python 3中input才变成默认接收str类型
-print ("功能介绍:\n")
-print ("\t1.退出:esc\n")
-print ("\t2.选择要安装的Device序号:index\n")
-print ("\t3.安装APK:任意输入\n")
+print("功能介绍:\n")
+print("\t1.退出:esc\n")
+print("\t2.选择要安装的Device序号:index\n")
+print("\t3.安装APK:任意输入\n")
 while 1:
     commond = input('>>>')
 
@@ -33,5 +33,5 @@ while 1:
         currentDeviceIndex = int(indexCom)
     else:
         iCom = input('\t>>>输入安装APK命令(省略adb -s前缀)\n')
-        print ('adb -s '+deviceArr[currentDeviceIndex] + ' ' + iCom)
-        os.system('adb -s '+deviceArr[currentDeviceIndex] + ' ' + iCom)
+        print('adb -s ' + deviceArr[currentDeviceIndex] + ' ' + iCom)
+        os.system('adb -s ' + deviceArr[currentDeviceIndex] + ' ' + iCom)
