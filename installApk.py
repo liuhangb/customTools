3860babdd66431d501510f922ec1025c03d85906
lh9 <>
2023-12-12 15:29:40 +0800

    【需求】更新安装apk的命令： 支持卸载并安装的操作, 并且简化使用流程

--- installApk.py
+++ installApk.py
@@ -1,37 +1,70 @@
 # coding=utf-8
 
 import os
+import re
+import sys
+import getopt
 
-# os.system不支持读取, 会直接输出控制台
-# os.system('adb devices')
-# os.popen支持读取
-ct = os.popen('adb devices').read()
-ctArr = ct.split("\n")
-deviceArr = []
+
+def showHint(deviceArr):
+    print("可选择的设备:" + str(deviceArr))
+    print("功能介绍:\n")
+    print("\t😈 选择要安装的Device序号:-x\n")
+    print("\t😈 安装应用前先卸载应用:-d\n")
+    print("\t😈 apk路径:-i\n")
+
+
+def queryDevices():
+    # os.system不支持读取, 会直接输出控制台
+    # os.system('adb devices')
+    # os.popen支持读取
+    ct = os.popen('adb devices').read()
+    ctArr = ct.split("\n")
+    deviceArr = []
+    for ele in ctArr:
+        if ele.endswith('device') == False:
+            continue
+
+        deviceArr.append(ele.split("\tdevice")[0])
+    return deviceArr
+
+
+def checkParam():
+    if apkPath == '':
+        exit("apkPath 不能为空")
+
+
+argv = sys.argv[1:]
+opts, args = getopt.getopt(argv, "x:i:d")  # 短选项模式
 currentDeviceIndex = 0
-for ele in ctArr:
-    if ele.endswith('device') == False:
-        continue
+apkPath = ''
+isFirstDelete = False
+deviceArr = queryDevices()
 
-    deviceArr.append(ele.split("\tdevice")[0])
-
-print("可选择的设备:" + str(deviceArr))
+if not opts:
+    showHint(deviceArr)
+    exit()
 # python 2.x中input如果需要接收str类型，必须输入时带"", 否则会报错
 # raw_input会将所有输入都当做字符串, 不需要输入时带""
 # python 3中input才变成默认接收str类型
-print("功能介绍:\n")
-print("\t1.退出:esc\n")
-print("\t2.选择要安装的Device序号:index\n")
-print("\t3.安装APK:任意输入\n")
-while 1:
-    commond = input('>>>')
-
-    if commond == 'esc':
-        break
-    if commond == 'index':
-        indexCom = input('\t>>>选择要安装的Device序号\n')
-        currentDeviceIndex = int(indexCom)
+for opt, arg in opts:
+    if opt in ['-x']:
+        currentDeviceIndex = arg
+    elif opt in ['-i']:
+        apkPath = arg
+    elif opt in ['-d']:
+        isFirstDelete = True
     else:
-        iCom = input('\t>>>输入安装APK命令(省略adb -s前缀)\n')
-        print('adb -s ' + deviceArr[currentDeviceIndex] + ' ' + iCom)
-        os.system('adb -s ' + deviceArr[currentDeviceIndex] + ' ' + iCom)
+        exit("未支持的参数:"+opt)
+
+checkParam()
+
+command = "adb -s " + deviceArr[currentDeviceIndex]
+if isFirstDelete:
+    # 先卸载
+    queryPkgCmd = "/Users/liuhang/Library/Android/sdk/build-tools/33.0.0/aapt2 dump badging " + apkPath
+    apkInfo = os.popen(queryPkgCmd)
+    packageName = re.findall(r"package: name=\'(.+?)\'", apkInfo.read())[0]
+    os.system(command + " uninstall " + packageName)
+
+os.system(command + " install " + apkPath)
